1b14044c0ffc20b03bc70f44d7ba5189099ccb3d
farah1999 <fdeeba1999@gmail.com>
2021-08-11 04:10:09 +0500

    Updated All

--- dev/index.php
+++ dev/index.php
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'https://synomart.com/dev/vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ require __DIR__.'https://synomart.com/dev/vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'https://synomart.com/dev/bootstrap/app.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
